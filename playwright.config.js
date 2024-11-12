a34c4b070821d0655e5100ce6fe0416374991bca
prajaktaka <studymailid12@gmail.com>
2024-11-12 15:15:23 +0530

    m

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  testMatch: '**/*.test.js',  // Pattern for test files
+  
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
